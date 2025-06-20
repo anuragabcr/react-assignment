c3af3c796f8631b1cdf566d2c8f49161171e8112
Anurag <anuragabcr@gmail.com>
2025-06-20 23:15:26 +0530

    Task 5 - Feature Implementation

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -6,6 +6,7 @@ import {
   ListItem,
   ListItemText,
   Snackbar,
+  Typography,
 } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { clearCart } from "./store/cartSlice";
@@ -15,6 +16,7 @@ type Product = {
   title: string;
   quantity: number;
   price: number;
+  discountPercentage?: number;
 };
 
 type CartProps = {
@@ -42,6 +44,20 @@ function Cart({
     }, 2000);
   };
 
+  const originalTotal = products.reduce(
+    (total, { price, quantity }) => total + price * quantity,
+    0
+  );
+
+  const discountedTotal = products.reduce(
+    (total, { price, quantity, discountPercentage = 0 }) => {
+      const discountedPrice = price - (price * discountPercentage) / 100;
+
+      return total + discountedPrice * quantity;
+    },
+    0
+  );
+
   return (
     <div>
       <h1>Shopping Cart</h1>
@@ -51,14 +67,23 @@ function Cart({
           <ListItem key={index}>
             <ListItemText
               primary={product.title}
-              secondary={"Quantity: " + product.quantity}
+              secondary={
+                `Quantity: ${product.quantity}` +
+                ` • Price ${product.price} ` +
+                (product.discountPercentage
+                  ? ` • Discount: ${product.discountPercentage}%`
+                  : "")
+              }
             />
           </ListItem>
         ))}
       </List>
-      <div>
-        Total Price: {products.reduce((total, { price }) => total + price, 0)}
-      </div>
+      <Typography variant="subtitle1" sx={{ mt: 2 }}>
+        <strong>Original Total:</strong> ₹{originalTotal.toFixed(2)}
+      </Typography>
+      <Typography variant="subtitle1" sx={{ mb: 2 }}>
+        <strong>Total after Discount:</strong> ₹{discountedTotal.toFixed(2)}
+      </Typography>
       {mode === "browse" ? (
         <Link to="/checkout">
           <Button style={{ marginBottom: 10 }} variant="contained">
